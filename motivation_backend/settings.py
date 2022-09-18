f1f43ad99d2fef4b1904c73ded8b4d3494faec89
Ivy Kabere <kabereivy@gmail.com>
2022-09-18 21:53:55 +0300

    initial setup

--- motivation_backend/settings.py
+++ motivation_backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -70,16 +72,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'motivation_backend.wsgi.application'
 
+AUTH_USER_MODEL = 'codersmotivation.User'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
+
+   }
 
 
 # Password validation
