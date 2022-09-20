0d5b7d58f665c857a532338e8f97c2d9a54283a8
Ivy Kabere <kabereivy@gmail.com>
2022-09-20 21:42:32 +0300

    move user model to authentication app

--- codersmotivation/models.py
+++ codersmotivation/models.py
@@ -1,15 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from cloudinary.models import CloudinaryField
+from authentication.models import User
 
 # Create your models here.
-class User(AbstractUser):
-  # Boolean fiels to select the type of account
-   is_admin = models.BooleanField(default=False)
-   is_staff= models.BooleanField(default=False)
 
-   def __str__(self):
-    return self.username
 
 class Category(models.Models):
   category=models.CharField(max_length=20)
