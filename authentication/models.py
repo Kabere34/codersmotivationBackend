d575bd264ba6275e11318d8c4c26c8f8dcbe1600
Ivy Kabere <kabereivy@gmail.com>
2023-03-23 07:32:47 +0300

    make changes to authentication model

--- authentication/models.py
+++ authentication/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-  # Boolean fiels to select the type of account
+  # Boolean fields to select the type of account
    is_admin = models.BooleanField(default=False)
    is_staff= models.BooleanField(default=False)
 
