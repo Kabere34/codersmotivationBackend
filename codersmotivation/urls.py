cf49d6234aaa017542ebb28d8a36e063b84237e6
Ivy Kabere <kabereivy@gmail.com>
2023-03-23 08:30:11 +0300

    add like view to add likes

--- codersmotivation/urls.py
+++ codersmotivation/urls.py
@@ -7,6 +7,10 @@ from authentication.views import LoginView
 urlpatterns = [
     path('', views.index, name="index"),
     path('api/post/',views.Postapi.as_view())
+    path('comments/', views.CommentAPIView.as_view(), name='comment_api'),
+    path('comments/<int:pk>', views.CommentAPIView.as_view(), name='comment_api_get'),
+    path('posts/<int:pk>/like/', LikeView.as_view()),
+    path('posts/<int:pk>/unlike/', UnlikeView.as_view()),
 ]
 
 
--- codersmotivation/views.py
+++ codersmotivation/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 import datetime
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Post as Post
-from .models import Profile
+from rest_framework.permissions import IsAuthenticated
+from .models import Post as Post, User, Comment
 from rest_framework import status
-from .serializer import PostSerializer, ProfileSerializer
+from .serializer import PostSerializer, CommentSerializer
+from rest_framework import generics, status
+from rest_framework.exceptions import ValidationError
+from django.contrib.contenttypes.models import ContentType
+from .serializer import LikeSerializer
+from .models import Profile
 
 # Create your views here.
 
@@ -24,3 +29,19 @@ class Postapi(APIView):
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LikeView(generics.UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def patch(self,request, *args, **kwargs):
+        instance=self.get_object()
+        instance.like +=1
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+
+
+
