e6c3ba3d83d16928720f401d9742e6b18b0eb18a
Ivy Kabere <kabereivy@gmail.com>
2023-03-23 07:30:33 +0300

    add post url

--- codersmotivation/urls.py
+++ codersmotivation/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from authentication.views import LoginView
+
+
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('api/post/',views.Postapi.as_view())
 ]
+
+
