4efdaf9f328a426b9bdd50702f1088d873103ab9
Ivy Kabere <kabereivy@gmail.com>
2023-03-23 07:28:22 +0300

    add login and registration urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+from authentication.views import LoginView
 
 urlpatterns={
   # Registration urls
   path('',views.index,name='index'),
-  path('login/',views.LoginView.as_view,name='login'),
-  path('logout/',views.LogoutView.as_view,name='logout')
+  path('/registration', views.UserCreate.as_view(), name="index"),
+  path('/login', LoginView.as_view(), name="login"),
+  # path('login/',views.LoginView.as_view,name='login'),
+  # path('logout/',views.LogoutView.as_view,name='logout')
 
 }
